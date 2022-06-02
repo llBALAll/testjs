4267c13ab9bdd03a8e872daea2b9a7de580da87d
llBALAll <43710708+llBALAll@users.noreply.github.com>
2022-06-02 04:35:45 -0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,14 @@
+// var funcSoma = require("./soma.js");
+// var funcSub = require("./subtracao.js");
+// var funcMult = require("./multiplicacao.js");
+// var funcDiv = require("./divisao.js");
+
+import { funcSoma } from './soma.js';
+// import { funcSub } from './subtracao.js';
+// import { funcMult } from './multiplicacao.js';
+// import { funcDiv } from './divisao.js';
+
+// console.log(funcSoma(1,2));
+// console.log(funcSub(10,5));
+// console.log(funcMult(100,10));
+// console.log(funcDiv(100,5));
